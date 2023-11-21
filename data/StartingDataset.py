37fdd18c5ea0b43ad8aadaeadaaef13ef25ba1c4
hsimington <haleighsreese@gmail.com>
2023-11-20 19:38:38 -0800

    fixed indexing of split data

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -11,24 +11,32 @@ class StartingDataset(torch.utils.data.Dataset):
     """
     Dataset that contains 21,397 color images of cassava leaves, size of image changed to 400x300x3.
     """
-    # "/Users/haleigh/haleigh/cassava-leaf-disease-classification/train.csv"
+    # Haleigh - "/Users/haleigh/haleigh/cassava-leaf-disease-classification/train.csv"
+    # Daisy - "C:/Users/dwatt/Downloads/cassava-leaf-disease-classification/train.csv"
 
     def __init__(self, train = False): 
-        self.dataframe = pd.read_csv("C:/Users/dwatt/Downloads/cassava-leaf-disease-classification/train.csv")
-        # FIRST ORDER OF BUSINESS 11/15 REDO SPLITTING OF DATA
+        self.dataframe = pd.read_csv("/Users/haleigh/haleigh/cassava-leaf-disease-classification/train.csv")
         if train == True:
+            self.offset = 0 # will add 0 to index line 34
             self.dataframe = self.dataframe.iloc[0:math.floor((0.8*len(self.dataframe)))]
         elif train == False:
+            self.offset = math.floor(0.8*len(self.dataframe)) # sets offset to the index of a random validation image
             self.dataframe = self.dataframe.iloc[math.floor(0.8*len(self.dataframe)):]
-        self.dataframe.reset_index()
+        self.dataframe.reset_index(drop=True) # resets index to default (iloc function)
+        # print(self.dataframe.head())
         self.image_id = self.dataframe["image_id"]
         self.label = self.dataframe["label"]
 
 
     def __getitem__(self, index):
+        # for validation dataset, index of a random validation image from dataset that contained training and validation set 
+        # is added to index of dataset that only contains validation set
+        index += self.offset
         image_id = self.image_id[index] # index is a built in function
+        # print("chk pt 1")
         label = self.label[index]
-        classification = (image_id, label)
+        # print("chk pt 2")
+        # classification = (image_id, label)
         im = Image.open('/Users/haleigh/haleigh/cassava-leaf-disease-classification/train_images/' + image_id)
         resize_img = im.resize((400, 300))
         convert_tensor = torchvision.transforms.ToTensor() # creates function that convert im tuple to tensor
--- train_functions/starting_train.py
+++ train_functions/starting_train.py
@@ -32,12 +32,13 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
     loss_fn = nn.CrossEntropyLoss()
 
     step = 0
-    model.train()
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1} of {epochs}")
         # Loop over each batch in the dataset
         for batch in tqdm(train_loader):
             # TODO: Backpropagation and gradient descent
+            model.train()
+
             images, labels = batch
            
             outputs = model(images)
@@ -72,14 +73,17 @@ def compute_accuracy(outputs, labels):
     Computes the accuracy of a model's predictions.
 
     Example input:
-        outputs: [0.7, 0.9, 0.3, 0.2]
-        labels:  [1, 1, 0, 1]
+        outputs: [[0,0,0.9,0,0.1], [0.2, ] ...]
+        labels:  [2,                1, ... ]
 
     Example output:
         0.75
     """
 
-    n_correct = (torch.round(outputs) == labels).sum().item()
+    # instead of rounding, we want to find index of max element (essentially which disease each leaf most likely has)
+    print(outputs.shape)
+    print(labels.shape)
+    n_correct = (torch.round(outputs) == labels).sum().item() # THIS NEEDS TO CHANGE 11/27
     n_total = len(outputs)
     return n_correct / n_total
 
@@ -99,7 +103,8 @@ def evaluate(loader, model, loss):
             
         outputs = model(images)
 
-        val_loss = loss(outputs, labels)
+        val_loss = loss(outputs, labels) # uses loss_fn above w/ val outputs and labels
             
+        # validation loss
         print('Val Loss: ', val_loss.item(), 'Val Accuracy: ', compute_accuracy(outputs, labels), '%')
     
