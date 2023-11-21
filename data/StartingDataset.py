53b23e223b5d76c03e8443ef2c5756174af530c7
daisywatters <sdaisywatters@g.ucla.edu>
2023-11-20 18:07:54 -0800

    determined use of 'index'

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -11,20 +11,22 @@ class StartingDataset(torch.utils.data.Dataset):
     """
     Dataset that contains 21,397 color images of cassava leaves, size of image changed to 400x300x3.
     """
+    # "/Users/haleigh/haleigh/cassava-leaf-disease-classification/train.csv"
 
     def __init__(self, train = False): 
-        self.dataframe = pd.read_csv("/Users/haleigh/haleigh/cassava-leaf-disease-classification/train.csv")
+        self.dataframe = pd.read_csv("C:/Users/dwatt/Downloads/cassava-leaf-disease-classification/train.csv")
         # FIRST ORDER OF BUSINESS 11/15 REDO SPLITTING OF DATA
         if train == True:
             self.dataframe = self.dataframe.iloc[0:math.floor((0.8*len(self.dataframe)))]
         elif train == False:
             self.dataframe = self.dataframe.iloc[math.floor(0.8*len(self.dataframe)):]
+        self.dataframe.reset_index()
         self.image_id = self.dataframe["image_id"]
         self.label = self.dataframe["label"]
 
 
     def __getitem__(self, index):
-        image_id = self.image_id[index] # IS THIS ACCESSING ALL DATA AS OPPOSED TO JUST TRAINING OR JUST VAL
+        image_id = self.image_id[index] # index is a built in function
         label = self.label[index]
         classification = (image_id, label)
         im = Image.open('/Users/haleigh/haleigh/cassava-leaf-disease-classification/train_images/' + image_id)
